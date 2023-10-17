1d0e679d847172be6b0ed7ce3725964425f98c1a
Rezwan66 <shaikhrezwan66@gmail.com>
2023-10-18 00:47:37 +0600

    created banner component with bg image overlay to the homepage

---
+++ src/components/Banner.jsx
@@ -0,0 +1,34 @@
+const Banner = () => {
+  return (
+    <div>
+      <div
+        className="hero h-[450px] bg-center rounded-md"
+        style={{
+          backgroundImage: 'url(/car-dashboard.jpg)',
+        }}
+      >
+        <div className="hero-overlay bg-opacity-60 rounded-md"></div>
+        <div className="hero-content w-full text-neutral-content text-right justify-end">
+          <div className="max-w-lg space-y-4">
+            <h1 className="text-5xl text-error font-bold">Drive the Future</h1>
+            <i className="font-bold text-white">
+              with Our Premier Automotive Brands
+            </i>
+            <br />
+            <p>
+              Experience the thrill of the open road with our elite selection of
+              automotive brands. From classic craftsmanship to eco-friendly
+              innovation, we have something for every car enthusiast. Start your
+              journey today and discover the epitome of automotive excellence.
+            </p>
+            <button className="btn btn-error btn-outline capitalize">
+              Ready, set, go!
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,9 @@
+import Banner from '../components/Banner';
+
 const Home = () => {
   return (
     <div>
-      <h2 className="text-3xl">Homepage</h2>
+      <Banner></Banner>
     </div>
   );
 };
