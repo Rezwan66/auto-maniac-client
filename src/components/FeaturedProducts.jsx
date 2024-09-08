e16382e26675af210fb3346aa861abc6d71c635c
Rezwan66 <shaikhrezwan66@gmail.com>
2024-09-08 19:50:50 +0200

    added a featured products section

---
+++ src/components/FeaturedProducts.jsx
@@ -0,0 +1,10 @@
+const FeaturedProducts = () => {
+  return (
+    <>
+      <div>
+        <h2> HELLO I Am FeaturedProducts </h2>
+      </div>
+    </>
+  );
+};
+export default FeaturedProducts;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,14 +4,38 @@ import BrandCard from '../components/BrandCard';
 import OurPrices from '../components/OurPrices';
 import Newsletter from '../components/Newsletter';
 import OurStats from '../components/OurStats';
+import { useEffect, useState } from 'react';
+import FeaturedProducts from '../components/FeaturedProducts';
 
 const Home = () => {
   const brands = useLoaderData();
   // console.log(brands);
+  const [products, setProducts] = useState();
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    fetch('https://auto-maniac-server.vercel.app/products')
+      .then(res => res.json())
+      .then(data => {
+        setProducts(data);
+        setLoading(false);
+      });
+  }, []);
+
+  console.log(products);
 
   return (
     <div>
       <Banner></Banner>
+      {/* our products */}
+      {loading ? (
+        <div className="text-center mt-20">
+          <span className="loading w-20 loading-spinner text-success"></span>
+          <span className="loading w-20 loading-spinner text-warning"></span>
+          <span className="loading w-20 loading-spinner text-secondary"></span>
+        </div>
+      ) : (
+        <FeaturedProducts />
+      )}
       {/* our brands */}
       <div className="my-10 mx-auto max-w-7xl">
         <h2 className="text-4xl font-semibold underline text-center mb-10">
